a40fe2930e26a8ce3cf5e3af6504541f6ba95cd7
Kunandiir <kunandiir@gmail.com>
2023-10-27 19:08:12 +0200

    complte task for module 8

---
+++ module_8/task/birthdays_inform.py
@@ -0,0 +1,47 @@
+from datetime import datetime,timedelta
+
+
+user_lst = [
+    {"name": "Alex", "birthday": datetime.today() + timedelta(days=0)}, 
+    {"name": "Oleg", "birthday": datetime.today() + timedelta(days=1)}, 
+    {"name": "Sasha", "birthday": datetime.today() + timedelta(days=2)}, 
+    {"name": "Nikita", "birthday": datetime.today() + timedelta(days=3)}, 
+    {"name": "Gleb", "birthday": datetime.today() + timedelta(days=4)}, 
+    {"name": "Maks", "birthday": datetime.today() + timedelta(days=5)}, 
+    {"name": "Hulio", "birthday": datetime.today() + timedelta(days=6)}
+]
+def get_birthdays_per_week(users):
+    today = datetime.today()
+    next_week = today + timedelta(weeks=1)
+    birth_users = {
+        "Monday": [],
+        "Tuesday": [],
+        "Wednesday": [],
+        "Thursday": [],
+        "Friday":[],
+
+    }
+    for user in users:
+        
+        user["birthday"] = user["birthday"].replace(year=today.year) #to ignore year
+        if today.date() <= user["birthday"].date() <= next_week.date():
+            if user["birthday"].strftime("%A") == "Saturday" or user["birthday"].strftime("%A") == "Sunday":
+                birth_users["Monday"].append(user["name"])
+            else:
+                birth_users[user["birthday"].strftime("%A")].append(user["name"])
+
+    print("\n" + "{:^35}".format("People You Should Congratulate\n"))
+    for key, values in birth_users.items():
+        print("{:^10}|{:^25}".format(key,",".join(values)))
+
+
+
+
+
+
+
+
+
+
+
+get_birthdays_per_week(user_lst)
