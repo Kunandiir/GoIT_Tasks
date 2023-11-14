29fddcb0c27b49dcc192ee3ef7889b7cce6abbf6
Kunandiir <kunandiir@gmail.com>
2023-11-14 10:09:53 +0100

    start module_10 task

---
+++ module_10/test/Adress_bot.py
@@ -0,0 +1,17 @@
+from collections import UserDict
+
+class AddressBook(UserDict):
+    print() 
+
+class Record():
+    print()
+
+class Field():
+    print()
+
+
+class Name():
+    print()
+
+class Phone():
+    print()
